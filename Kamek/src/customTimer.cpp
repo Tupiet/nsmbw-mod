4a24f678a0a995be21ed85513b6da96f27fff9c6
Tupiet <aleixescrihuela@gmail.com>
2021-06-02 23:48:28 +0200

    Some updates

--- Kamek/src/customTimer.cpp
+++ Kamek/src/customTimer.cpp
@@ -3,6 +3,11 @@
 #include <g3dhax.h>
 #include <sfx.h>
 
+const char* TCarcNameList [] = {
+	"clock",
+	NULL
+};
+
 extern "C" bool SpawnEffect(const char*, int, Vec*, S16Vec*, Vec*);
 extern u32 GameTimer;
 
@@ -124,15 +129,15 @@ int daCustomTimer::onCreate() {
 
 	allocator.link(-1, GameHeaps[0], 0, 0x20);
 
-	nw4r::g3d::ResFile rf(getResource("star_coin", "g3d/star_coin.brres"));
-	bodyModel.setup(rf.GetResMdl("star_coinA"), &allocator, 0x224, 1, 0);
+	nw4r::g3d::ResFile rf(getResource("clock", "g3d/clock.brres"));
+	bodyModel.setup(rf.GetResMdl("ClockBlue"), &allocator, 0x224, 1, 0);
 	SetupTextures_MapObj(&bodyModel, 0);
 
 	allocator.unlink();
 
 	ActivePhysics::Info HitMeBaby;
 	HitMeBaby.xDistToCenter = 0.0;
-	HitMeBaby.yDistToCenter = -3.0;
+	HitMeBaby.yDistToCenter = 0.0;
 	HitMeBaby.xDistToEdge = 12.0;
 	HitMeBaby.yDistToEdge = 15.0;
 	HitMeBaby.category1 = 0x5;
@@ -145,11 +150,10 @@ int daCustomTimer::onCreate() {
 	this->aPhysics.initWithStruct(this, &HitMeBaby);
 	this->aPhysics.addToList();
 
-	this->scale.x = 1.0;
-	this->scale.y = 1.0;
-	this->scale.z = 1.0;
+	this->scale.x = 50.0;
+	this->scale.y = 50.0;
+	this->scale.z = 50.0;
 
-	this->pos.x -= 120.0;
 	this->pos.z = 3300.0;
 
 	this->onExecute();
