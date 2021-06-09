28fcb4df610ca3a371579eb2673de1d14597ff5a
Tupiet <aleixescrihuela@gmail.com>
2021-06-09 22:14:42 +0200

    Trying to add gakenoko

--- Kamek/src/customTimer.cpp
+++ Kamek/src/customTimer.cpp
@@ -11,7 +11,7 @@ const char* TCarcNameList [] = {
 extern "C" bool SpawnEffect(const char*, int, Vec*, S16Vec*, Vec*);
 extern u32 GameTimer;
 
-#define time *(u32*) ((GameTimer) + 0x4)
+#define timeActual *(u32*) ((GameTimer) + 0x4)
 
 
 class daCustomTimer : public dEn_c {
@@ -49,9 +49,29 @@ class daCustomTimer : public dEn_c {
 
 };
 
-void daCustomTimer::playerCollision(ActivePhysics *apThis, ActivePhysics *apOther) {
+class TimeKeeper {
+    void *vtable; // 0x8031B358
+    u32 timePlusFFFTimes40000; // not sure why this is here...
+    u16 time;
+    u8 _A;
+    u8 isTimeLessThan100;
+    u8 _C;
 
-	time = time + (20 << 0xC) - 1;
+    public: static TimeKeeper* instance; // 0x8042A350
+
+    TimeKeeper(u32 *buffer, u32 initialTime); // 0x800E38E0
+    virtual ~TimeKeeper(); // 0x800E3910
+
+    public: void setTime(u32 time); // 0x800E3A00 - updates both this->time and the u32. Maybe you need to manually set _B...
+    public: void updateUI(); // 0x800E3A20 - updates the display and speeds up the music if needed.
+
+    void sub_800E3B50(); // 0x800E3B50 - related to time up
+};
+
+void daCustomTimer::playerCollision(ActivePhysics *apThis, ActivePhysics *apOther) {
+	TimeKeeper::instance->setTime(124);
+	TimeKeeper::instance->updateUI();
+	//timeActual = timeActual + (20 << 0xC) - 1;
 
 	//FIXME changed to dStageActor_c::Delete(u8) from fBase_c::Delete(void)
 	this->Delete(1);
